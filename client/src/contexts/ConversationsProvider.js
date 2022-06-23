c15253e8ceb4fb91ea483077b44a582eebb4a534
James Caleca <jacaleca2@gmail.com>
2022-06-22 21:20:14 -0600

    More changes in ConversationsProvider

--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -11,14 +11,27 @@ export function useConversations() {
 export function ConversationsProvider({ children }) {
   const [conversations, setConversations] = useLocalStorage('conversations', [])
 
+  const { contacts } = useContacts()
+
   function createConversation(recipients) {
     setConversations(prevConversations => {
       return [...prevConversations, { recipients, messages: [] }]
     })
   }
 
+  const formattedConversations = conversations.map(conversation => {
+    const recipients = conversation.recipients.map(recipient => {
+        const contact = contacts.find(contact => {
+            return contact.id === recipient
+        })
+        const name = (contact && contact.name) || recipient
+        return { id: recipient, name }
+    })
+    return { ... conversation, recipients }
+  })
+
   return (
-    <ConversationsContext.Provider value={{ conversations, createConversation }}>
+    <ConversationsContext.Provider value={{ conversations: formattedConversations, createConversation }}>
       {children}
     </ConversationsContext.Provider>
   )
