938a07952fab001d967fb2ec2599184f9e9bf7a4
James Caleca <jacaleca2@gmail.com>
2022-06-23 20:34:59 -0600

    More changes to conversation providers and created new components

--- client/src/contexts/ConversationsProvider.js
+++ client/src/contexts/ConversationsProvider.js
@@ -1,6 +1,6 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import useLocalStorage from '../hooks/useLocalStorage'
-// import { ContactsContext } from './ContactsProvider'
+import { useContacts } from './ContactsProvider'
 
 const ConversationsContext = React.createContext()
 
@@ -10,7 +10,7 @@ export function useConversations() {
 
 export function ConversationsProvider({ children }) {
   const [conversations, setConversations] = useLocalStorage('conversations', [])
-
+  const [selectedConversationIndex, setSelectedConversationIndex] = useState(0)
   const { contacts } = useContacts()
 
   function createConversation(recipients) {
@@ -19,7 +19,7 @@ export function ConversationsProvider({ children }) {
     })
   }
 
-  const formattedConversations = conversations.map(conversation => {
+  const formattedConversations = conversations.map((conversation, index) => {
     const recipients = conversation.recipients.map(recipient => {
         const contact = contacts.find(contact => {
             return contact.id === recipient
@@ -27,11 +27,19 @@ export function ConversationsProvider({ children }) {
         const name = (contact && contact.name) || recipient
         return { id: recipient, name }
     })
-    return { ... conversation, recipients }
+    const selected = index === selectedConversationIndex
+    return { ...conversation, recipients, selected }
   })
 
+  const value = {
+    conversations: formattedConversations, 
+    selectedConversation: formattedConversations[selectedConversationIndex],
+    selectConversationIndex: setSelectedConversationIndex,
+    createConversation
+  }
+
   return (
-    <ConversationsContext.Provider value={{ conversations: formattedConversations, createConversation }}>
+    <ConversationsContext.Provider value={value}>
       {children}
     </ConversationsContext.Provider>
   )
